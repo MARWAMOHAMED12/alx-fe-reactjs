ff13a10fd20989ae1c03f99c96f2ec0ea957b8aa
MARWAMOHAMED12 <5849hamada@gmail.com>
2025-08-16 21:50:28 +0300

    update: implemented UserContext and integrated it with App and UserProfile components

--- alx-react-app-new/src/components/UserProfile.jsx
+++ alx-react-app-new/src/components/UserProfile.jsx
@@ -1,9 +1,13 @@
-function UserProfile(props) {
+import React, { useContext } from "react";
+import { UserContext } from "../UserContext";
+
+function UserProfile() {
+  const userData = useContext(UserContext);
+
   return (
-    <div style={{ border: '1px solid gray', padding: '10px', margin: '10px', borderRadius: '8px' }}>
-      <h2 style={{ color: 'blue' }}>{props.name}</h2>
-      <p>Age: <span style={{ fontWeight: 'bold' }}>{props.age}</span></p>
-      <p style={{ fontStyle: 'italic' }}>Bio: {props.bio}</p>
+    <div>
+      <h2>{userData.name}</h2>
+      <p>{userData.email}</p>
     </div>
   );
 }
