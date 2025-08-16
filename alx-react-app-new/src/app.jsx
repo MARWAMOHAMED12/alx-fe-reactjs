8e10fc45fb298c9a3c0afbc56d1a0de6a9e0088b
MARWAMOHAMED12 <5849hamada@gmail.com>
2025-08-16 21:20:04 +0300

    create: add Counter component with increment, decrement, and reset functionality

--- alx-react-app-new/src/app.jsx
+++ alx-react-app-new/src/app.jsx
@@ -2,6 +2,7 @@ import Header from './components/Header';
 import UserProfile from './components/UserProfile';
 import MainContent from './components/MainContent';
 import Footer from './components/Footer';
+import Counter from './components/Counter'; // ✅ ضيف الاستيراد الجديد
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Header />
       <UserProfile name="Marwa" age="22" bio="I love coding and learning new things!" />
       <MainContent />
+      <Counter />   {/* ✅ عرض الكاونتر */}
       <Footer />
     </div>
   );
